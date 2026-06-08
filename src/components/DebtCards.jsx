4e7acd01124685e1ca3e013f31b1097e57b1a2f8
Rahil69 <raahil2005@hotmail.com>
2026-06-08 13:43:52 +0500

    updated debt cards styling

--- src/components/DebtCards.jsx
+++ src/components/DebtCards.jsx
@@ -5,12 +5,14 @@ const DebtCards = ({ title, amount, subtitle, colour }) => {
 
   return (
     <div className="rounded-3xl border border-white/80 bg-white/70 p-5 backdrop-blur-xl">
-      <div className="flex items-center gap-2">
-        <div className="rounded-xl  p-2">
-          <Icon size={18} className="text-neutral-600" />
+      <div className="flex items-center ">
+        <div className="rounded-xl  p-2 ">
+          <Icon size={18} className="text-emerald-900" />
         </div>
 
-        <p className="text-xs tracking-wide text-black">{title}</p>
+        <p className="text-sm tracking-wide text-black font-semibold">
+          {title}
+        </p>
       </div>
       <p className={`mt-3 text-lg font-semibold ${colour}`}>{amount}</p>
       <p className="mt-1 text-xs text-neutral-600">{subtitle}</p>
