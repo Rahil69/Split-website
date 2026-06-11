238a17128a1c3124e2846286aaf66ab0d23b2199
Rahil69 <raahil2005@hotmail.com>
2026-06-11 18:49:09 +0500

    navbar changes (final hopefully)

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -2,7 +2,7 @@ import { Home, Utensils } from "lucide-react";
 
 function BottomNav(props) {
   return (
-    <nav className="fixed bottom-6 left-1/2 w-45 max-w-sm -translate-x-1/2 rounded-full border border-white/70 bg-white/10 p-1 shadow-[0_12px_28px_rgba(15,23,42,0.12),inset_0_1px_1px_rgba(255,255,255,0.3)] backdrop-blur-sm">
+    <nav className="fixed bottom-6 left-1/2 w-45 max-w-sm -translate-x-1/2 rounded-full border border-white/10 bg-white/10 p-1 shadow-[0_12px_28px_rgba(15,23,42,0.12),inset_0_1px_1px_rgba(255,255,255,0.3)] backdrop-blur-sm">
       <div className="grid grid-cols-2 gap-1">
         <button
           onClick={() => props.setActivePage("Home")}
