3d3238515c9577a6131efe298d01f95ff06108ae
Rahil69 <raahil2005@hotmail.com>
2026-06-11 13:55:15 +0500

    navbar ui changes

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -2,28 +2,40 @@ import { Home, Utensils } from "lucide-react";
 
 function BottomNav(props) {
   return (
-    <nav className="fixed bottom-6 left-1/2 w-50  max-w-sm -translate-x-1/2 rounded-full border border-white/50 bg-white/[0.04] p-2 shadow-[0_10px_24px_rgba(24,84,63,0.08)] backdrop-blur-2xl">
-      <div className="grid grid-cols-2 gap-2 ">
+    <nav className="fixed bottom-6 left-1/2 w-52 max-w-sm -translate-x-1/2 rounded-full border border-white/50 !bg-white/20 p-1.5 shadow-[0_10px_24px_rgba(24,84,63,0.08)] backdrop-blur-md">
+      <div className="grid grid-cols-2 gap-0 ">
         <button
           onClick={() => props.setActivePage("Home")}
-          className={`flex items-center justify-center gap-2 rounded-full px-5 py-2 text-sm font-extrabold transition ${
-            props.activePage === "Home"
-              ? "bg-white/75 text-black shadow-sm shadow-black/5"
-              : "text-black/60"
+          className={`flex items-center justify-center rounded-full px-4 py-1 text-sm font-extrabold transition ${
+            props.activePage === "Home" ? "text-black" : "text-black/60"
           }`}
         >
-          <Home size={20} />
+          <span
+            className={`flex h-10 w-16 items-center justify-center rounded-full transition ${
+              props.activePage === "Home"
+                ? "bg-white/90 shadow-md  ring-black/5"
+                : ""
+            }`}
+          >
+            <Home size={20} />
+          </span>
         </button>
 
         <button
           onClick={() => props.setActivePage("Split")}
-          className={`flex items-center justify-center gap-2 rounded-full px-5 py-3 text-sm font-extrabold transition ${
-            props.activePage === "Split"
-              ? "bg-white/75 text-black shadow-sm shadow-black/5"
-              : "text-black/60"
+          className={`flex items-center justify-center rounded-full px-4 py-1 text-sm font-extrabold transition ${
+            props.activePage === "Split" ? "text-black" : "text-black/60"
           }`}
         >
-          <Utensils size={20} />
+          <span
+            className={`flex h-10 w-16 items-center justify-center rounded-full transition ${
+              props.activePage === "Split"
+                ? "bg-white/90 shadow-md  ring-black/5"
+                : ""
+            }`}
+          >
+            <Utensils size={20} />
+          </span>
         </button>
       </div>
     </nav>
@@ -31,4 +43,3 @@ function BottomNav(props) {
 }
 
 export default BottomNav;
-
