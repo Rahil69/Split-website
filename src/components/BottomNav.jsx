1c24ca51cfeca2bbd7fe1a2a40c704d91187bc9d
Rahil69 <raahil2005@hotmail.com>
2026-06-11 14:01:38 +0500

    navbar ui changes

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -2,7 +2,7 @@ import { Home, Utensils } from "lucide-react";
 
 function BottomNav(props) {
   return (
-    <nav className="fixed bottom-6 left-1/2 w-52 max-w-sm -translate-x-1/2 rounded-full border border-white/50 !bg-white/20 p-1.5 shadow-[0_10px_24px_rgba(24,84,63,0.08)] backdrop-blur-md">
+    <nav className="fixed bottom-6 left-1/2 w-52 max-w-sm -translate-x-1/2 rounded-full border border-white/10 !bg-white/1 p-1.5 shadow-[0_10px_24px_rgba(24,84,63,0.08)] backdrop-blur-sm">
       <div className="grid grid-cols-2 gap-0 ">
         <button
           onClick={() => props.setActivePage("Home")}
@@ -30,7 +30,7 @@ function BottomNav(props) {
           <span
             className={`flex h-10 w-16 items-center justify-center rounded-full transition ${
               props.activePage === "Split"
-                ? "bg-white/90 shadow-md  ring-black/5"
+                ? "bg-white/20 shadow-md  ring-black/5"
                 : ""
             }`}
           >
